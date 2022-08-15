6ae4509e1049196ca0171c2491ead37c7eb3ef4d
laboneeaktar <labonipub@gmail.com>
2022-08-15 07:49:15 +0600

    first commit

---
+++ js/panda.js
@@ -0,0 +1,7 @@
+const allh2Tags = document.getElementsByTagName('h2');
+for (const h2 of allh2Tags) {
+    h2.style.color = 'red';
+}
+
+const backPack = document.getElementById('backpack');
+backPack.style.backgroundColor = 'tomato';
