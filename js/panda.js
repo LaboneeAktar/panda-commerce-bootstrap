db583d5a922429533cd1f47c172968f826609382
laboneeaktar <labonipub@gmail.com>
2022-08-15 08:00:14 +0600

    sothing remove

--- js/panda.js
+++
@@ -1,7 +0,0 @@
-const allh2Tags = document.getElementsByTagName('h2');
-for (const h2 of allh2Tags) {
-    h2.style.color = 'red';
-}
-
-const backPack = document.getElementById('backpack');
-backPack.style.backgroundColor = 'tomato';
